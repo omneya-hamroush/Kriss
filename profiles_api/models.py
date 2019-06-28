362fd41e258a3ff2d0ebe9b0296ba0cdd4ff1e9d
ahmed hamroush <ahmedhamroush@ahmeds-MacBook-Pro.local>
2019-06-28 22:14:59 +0200

    third commit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+
+
+class Product (models.Model):
+     name = models.CharField(max_length=255)
+     description=models.TextField(max_length=2000)
+     price=models.DecimalField(max_digits=10, decimal_places=2)
+
+
+     REQUIRED_FIELDS= ['name']
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.views.static import serve
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
