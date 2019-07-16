cbaa36b75dfb4201313cb3465590d418cc32ef64
ahmed hamroush <ahmedhamroush@ahmeds-MacBook-Pro.local>
2019-07-16 15:01:16 +0200

    commits

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -89,8 +89,16 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'mysql.connector.django',
+        'NAME': 'Kriss',
+        'USER': 'root',
+        'PASSWORD': 'BIT68_123qwe4r',
+        # 'HOST': 'kriss.bit68.com',
+        # 'PORT': 8000,
     }
 }
 
